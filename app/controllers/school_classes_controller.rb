13674f8a993b5066067abcefeb9194f1a082d63a
Teerth Patel <teerth1205@gmail.com>
2020-03-29 22:03:47 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -6,7 +6,7 @@ class SchoolClassesController < ApplicationController
   def create
     @school_class = SchoolClass.new(school_class_params(:title, :room_number))
     @school_class.save
-    redirect_to schoolclass_path(@school_class)
+    redirect_to school_class_path(@school_class)
   end
 
   def show
@@ -20,7 +20,7 @@ class SchoolClassesController < ApplicationController
   def update
     @school_class = SchoolClass.find(params[:id])
     @school_class.update(school_class_params(:title, :room_number))
-    redirect_to schoolclass_path(@school_class)
+    redirect_to school_class_path(@school_class)
   end
 
 private
