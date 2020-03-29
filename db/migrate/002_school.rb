2434d5e5eb5895092d66512761d19fea1cbb3588
Teerth Patel <teerth1205@gmail.com>
2020-03-29 22:05:26 +0000

    Done.

---
+++ db/migrate/002_school.rb
@@ -0,0 +1,10 @@
+class School < ActiveRecord::Migration
+  def change
+    create_table :school_classes do |t|
+      t.string :title
+      t.string :room_number
+
+      t.timestamps null: false
+    end
+  end
+end
