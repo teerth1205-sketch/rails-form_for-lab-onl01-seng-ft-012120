ceb302c134d5ba4083d9c1e864fe99e8b37c706e
Teerth Patel <teerth1205@gmail.com>
2020-03-29 22:05:10 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -20,7 +20,7 @@ class SchoolClassesController < ApplicationController
   def update
     @school_class = SchoolClass.find(params[:id])
     @school_class.update(school_class_params(:title, :room_number))
-    redirect_to schoolclass_path(@school_class)
+    redirect_to school_class_path(@school_class)
   end
 
 private
